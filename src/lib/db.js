3dd6a04f005b1b7c7e16badaca3c271dc72678dd
Rahul <002rahul11@gmail.com>
2025-09-03 15:57:21 +0530

    Updated

--- src/lib/db.js
+++ src/lib/db.js
@@ -6,6 +6,7 @@ export async function getConnection() {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT || 3306
+    port: process.env.DB_PORT || 3306,
+    ssl: { rejectUnauthorized: true }
   });
 }
